a2a1e4447de884205e97bdb6e6b73f389b3ad622
Djoufson CHE BENE <djouflegran@gmail.com>
2024-10-11 22:08:30 +0100

    style[client]: add the delete todo ui

--- client/src/components/TodoItem.vue
+++ client/src/components/TodoItem.vue
@@ -2,6 +2,8 @@
 import { createTodo, updateTodo } from '@/services/api';
 import type { Todo } from '@/types/Todo';
 import { emptyTodo } from '@/utilities/todoUtilities';
+import { formatDate } from '@/utilities/dateUtilities';
+import { Icon } from '@iconify/vue';
 import { ref } from 'vue';
 
 const props = defineProps<{
@@ -23,15 +25,17 @@ function onSave() {
 </script>
 
 <template>
-  <div class="flex gap-4 w-full border-2 border-blue-200 rounded-lg p-2 items-center">
+  <div class="todo-item flex gap-4 w-full border-2 border-blue-200 rounded-lg p-2 items-center">
     <input @keydown.enter="onSave" type="text" placeholder="Add a new task" @focusin="isFocus = true" @focusout="isFocus = false" :value="todo?.title" class="bg-transparent p-2 w-full focus:outline-none">
-    <div v-if="isFocus">
-      <div v-if="todo">
-        <button>Save</button>
-      </div>
-      <div v-else>
-        <button>Add</button>
-      </div>
+    <div
+      v-if="props.todo"
+      class="todo-action w-[50px] flex items-center justify-center">
+      <button class="w-full flex items-center justify-center text-red-300 hover:text-red-400 transition-all">
+        <Icon
+          width="60%"
+          icon="mdi-light:delete"/>
+      </button>
     </div>
   </div>
+  <span v-if="props.todo" class="ml-3 text-xs">{{ formatDate(todo?.dueDate ?? new Date()) }}</span>
 </template>
